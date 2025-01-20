0df7245829c5aac24bf1cdd38fb3965c76bca000
Mahir Topcic <mahir.topcic@edu.fit.ba>
2025-01-20 19:54:37 +0100

    user search

---
+++ miniJobs/Application/Users/Handlers/UserSearchQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Models;
+using Application.Users.Queries;
+using Domain.Entities;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Users.Handlers;
+
+sealed class UserSearchQueryHandler(IUserManagerRepository userManagerRepository) : IRequestHandler<UserSearchQuery, SearchResponseBase<User>>
+{
+    public async Task<SearchResponseBase<User>> Handle(UserSearchQuery request, CancellationToken cancellationToken)
+    {
+        var results = new SearchResponseBase<User>();
+        
+        results.Result = await userManagerRepository.FindAllAsync();
+        results.Count =( await userManagerRepository.FindAllAsync()).Count();
+        return results;
+    }
+}
---
+++ miniJobs/Application/Users/Queries/UserSearchQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Models;
+using Application.Common.Queries;
+using Domain.Entities;
+
+namespace Application.Users.Queries;
+
+public class UserSearchQuery(Dictionary<string, string> parameters) : QueryBase<SearchResponseBase<User>>
+{
+    public Dictionary<string, string> Parameters { get; set; } = parameters;
+}
