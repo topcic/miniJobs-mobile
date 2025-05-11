caf47efddc5f7a14ac5684e72bf75fa1c6e97626
mahir <Mahir@ARNI-YOGA>
2025-05-11 22:18:00 +0200

    .

--- miniJobs/UI/minijobs_admin/lib/pages/auth/verification_page.dart
+++ miniJobs/UI/minijobs_admin/lib/pages/auth/verification_page.dart
@@ -93,7 +93,7 @@ class _VerificationPageState extends State<VerificationPage> {
                         var result =
                         await _authenticationProvider.tokens(authCodeRequest);
 
-                        if (result!=null) {
+                        if (result!=null  && result!=false) {
                           notificationService.success("Uspješno ste potvrdili Vaš email. Molimo Vas prijavite se.");
                           Navigator.of(context).push(MaterialPageRoute(
                             builder: (context) => const LoginPage(),
