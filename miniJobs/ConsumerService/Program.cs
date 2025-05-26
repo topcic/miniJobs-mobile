febb794d9ab374a5c6644ffb7277188bb756c231
mahir <Mahir@ARNI-YOGA>
2025-05-26 08:29:33 +0200

    test

--- miniJobs/ConsumerService/Program.cs
+++ miniJobs/ConsumerService/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ConsumerService.Consumers;
 using MassTransit;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ConsumerService;
+using ConsumerService.Options; // Za EmailService i EmailSender
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
@@ -12,15 +15,20 @@ var host = Host.CreateDefaultBuilder(args)
             configure.SetKebabCaseEndpointNameFormatter();
             configure.UsingRabbitMq((ctx, cfg) =>
             {
-                cfg.Host(new Uri(context.Configuration["RabbitMQ:Host"]!), h =>
+                cfg.Host(new Uri(context.Configuration["RABBITMQ:Host"]!), h =>
                 {
-                    h.Username(context.Configuration["RabbitMQ:Username"]!);
-                    h.Password(context.Configuration["RabbitMQ:Password"]!);
+                    h.Username(context.Configuration["RABBITMQ:Username"]!);
+                    h.Password(context.Configuration["RABBITMQ:Password"]!);
                 });
                 cfg.ReceiveEndpoint("application-expiry-email-queue", e => e.ConfigureConsumer<ApplicationExpiryEmailConsumer>(ctx));
                 cfg.ReceiveEndpoint("job-recommendation-email-queue", e => e.ConfigureConsumer<JobRecommendationEmailConsumer>(ctx));
             });
         });
+
+        // Registriraj EmailService i EmailSender
+        services.Configure<EmailSenderOptions>(context.Configuration.GetSection("EmailSenderOptions"));
+        services.AddSingleton<EmailService>();
+        services.AddSingleton<EmailSender>();
     })
     .Build();
 
